9bd61749422cb5ce15ca07a0d81357d390283486
juanzolotoochin <88746611+juanzolotoochin@users.noreply.github.com>
2026-06-13 12:36:25 -0700

    docs(rust): spec for GPUI hello-world example (#23)
    GPUI Example

---
+++ examples/rust/gpui_hello_world/src/main.rs
@@ -0,0 +1,37 @@
+use gpui::{
+    div, prelude::*, px, rgb, size, App, Application, Bounds, Context, SharedString, Window,
+    WindowBounds, WindowOptions,
+};
+
+struct HelloWorld {
+    text: SharedString,
+}
+
+impl Render for HelloWorld {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .bg(rgb(0x2e2e2e))
+            .size_full()
+            .justify_center()
+            .items_center()
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
+    }
+}
+
+fn main() {
+    Application::new().run(|cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(300.), px(300.)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |_, cx| cx.new(|_| HelloWorld { text: "World".into() }),
+        )
+        .unwrap();
+        cx.activate(true);
+    });
+}
